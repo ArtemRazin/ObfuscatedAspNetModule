2b5301161747eb89c7c2d97c57f761957ee29c00
Artem Razin <razin@softanics.com>
2025-04-22 17:32:30 +0300

    Initial version

---
+++ SimpleModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web;
+
+using ArmDot.Client;
+
+[assembly: EmbedFile(SourcePath: "file_content.txt", RuntimePath: @"$(AssemblyDirectory)\embedded_file.txt")]
+
+[assembly: ArmDot.Client.VirtualizeCode]
+
+namespace SimpleModule
+{
+    public class SimpleModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += new EventHandler(OnBeginRequest);
+        }
+
+        private void OnBeginRequest(object sender, EventArgs e)
+        {
+            HttpApplication app = (HttpApplication)sender;
+            app.Context.Response.Write("<p>SimpleModule: BeginRequest triggered !</p>");
+        }
+
+        public void Dispose()
+        {
+            // Clean-up code if needed
+        }
+    }
+}
